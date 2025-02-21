f59439a04246aba4ea63d4b60428dfd2090676a6
Mauricio Ayllón Varela <mauri051928@gmail.com>
2025-02-20 22:16:52 -0500

    Database and project initialized

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { DatabaseModule } from '@/database/database.module';
+import { ConfigModule } from '@nestjs/config';
+// import { ApiModule } from '@/api/api.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    // ApiModule,
+    DatabaseModule
+  ],
+})
+  
+export class AppModule {}
