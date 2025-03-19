3a9e338b059972cffc28ee9c1192636fec187420
Mauricio Ayllón Varela <mauri051928@gmail.com>
2025-03-19 00:17:56 -0500

    feat: added cron to delete users deleted from db every year

--- src/jobs/services/token-cleanup.service.ts
+++ src/jobs/services/token-cleanup.service.ts
@@ -1,4 +1,5 @@
 import { Token } from "@/database/entities/token.entity";
+import { User } from "@/database/entities/user.entity";
 import { Injectable } from "@nestjs/common";
 import { Cron, CronExpression } from "@nestjs/schedule";
 import { InjectRepository } from "@nestjs/typeorm";
@@ -9,12 +10,14 @@ export class TokenCleanupService {
   constructor(
     @InjectRepository(Token)
     private readonly tokenRepository: Repository<Token>,
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
   ) { }
   
   @Cron(CronExpression.EVERY_DAY_AT_3AM, {
     name: "expired-token"
   })
-  async removedExpiredTokens() { 
+  async removeExpiredTokens() { 
     try {
       const nowDate = new Date()
       const { affected } = await this.tokenRepository.delete({ expiresAt: LessThan(nowDate)})
@@ -22,6 +25,19 @@ export class TokenCleanupService {
       console.log(`[${new Date().toISOString()}] ✅ Expired tokens removed: ${affected}`);
     } catch (error) {
       console.error(`[${new Date().toISOString()}]`)
+    }
+  }
+
+  @Cron(CronExpression.EVERY_YEAR)
+  async removeDeletedAccounts() {
+    try {
+      const { affected } = await this.userRepository.delete({
+        deleteAt: LessThan(new Date()), 
+      });
+
+      console.log(`[${new Date().toISOString()}] ✅ Deleted accounts removed: ${affected}`);
+    } catch (error) {
+      console.error(`[${new Date().toISOString()}] ❌ Error removing deleted accounts:`, error);
     }
   }
 }
